f0c99ff1bd12bb709814330e1eb4206e2cad5b51
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-04-27 12:30:52 +0200

    Created BalanceHeader.kt

--- app/src/main/java/com/example/wokolskidashboard/ui/components/BalanceHeader.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/BalanceHeader.kt
@@ -1,1 +1,20 @@
 package com.example.wokolskidashboard.ui.components
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun BalanceHeader(
+    saldo: Double
+){
+    Column(modifier = Modifier.padding(0.dp,10.dp,0.dp,0.dp)){
+        Text("$saldo Rubli", fontSize = 30.sp)
+    }
+
+}
+
