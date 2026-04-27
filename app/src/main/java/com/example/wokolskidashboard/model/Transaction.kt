a1fe1cdacd730843b67c13c6008ecb03b23b7929
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-04-27 14:32:57 +0200

    removed unused values

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -3,6 +3,5 @@ package com.example.wokolskidashboard.model
 data class Transaction(
     val name: String,
     val amount: Double,
-    val isIncome: Boolean,
-    var isImportant: Boolean
+    val isIncome: Boolean
 )
