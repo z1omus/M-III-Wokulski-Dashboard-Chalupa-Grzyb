880f8f44c30e47805b9e8ba8e58c5d472dc23579
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-06-01 23:25:22 +0200

    Update Transaction.kt

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -2,4 +2,5 @@ package com.example.wokolskidashboard.model
 data class Transaction(
     val name: String,
     val amount: Double,
-    val isIncome: Boolean)
+    val isIncome: Boolean
+    var isImportant: Boolean)
