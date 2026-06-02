251cb4c569962ea47e15d1a089acf8ddf0f2e3b0
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-06-02 08:36:32 +0200

    Update TransactionCard.kt

--- app/src/main/java/com/example/wokolskidashboard/ui/components/TransactionCard.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/TransactionCard.kt
@@ -23,9 +23,9 @@ fun TransactionCard(
         }  else {
             Text("${transaction.name} - ${transaction.amount} - ${
                 if(transaction.isImportant){
-                    "Niezbyteczny"
-                }  else {
                     "Zbyteczny"
+                }  else {
+                    "Niezbyteczny"
                 }
             }")
         }
